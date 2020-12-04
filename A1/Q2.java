cf7bca1baa72bb5ca6f09dacd9c892c281da3c6b
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-12-04 21:33:01 +0530

    Create Q2.java

---
+++ A1/Q2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main
+{
+	static double squareRoot(double n, double l)
+	{
+		double x = n;
+		double root;
+		int count = 0;
+		while (true)
+		{
+			count++;
+			root = 0.5 * (x + (n / x));
+			if (Math.abs(root - x) < l)
+				break;
+			x = root;
+		}
+		
+		return root;
+	}
+	
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		double l = 0.000001;
+		System.out.println(squareRoot(n, l));
+		sc.close();
+	}
+}
